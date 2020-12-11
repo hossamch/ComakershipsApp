015769161b7bf8065277c5adb371d104071473b9
Hossam Chakroun <hossam_c@icloud.com>
2020-12-11 01:20:04 +0100

    Initial Commit

---
+++ ComakershipsApp/ComakershipsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ComakershipsAppApp.swift
+//  ComakershipsApp
+//
+//  Created by Hossam Chakroun on 11/12/2020.
+//
+
+import SwiftUI
+
+@main
+struct ComakershipsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ComakershipsAppTests/ComakershipsAppTests.swift
@@ -0,0 +1,33 @@
+//
+//  ComakershipsAppTests.swift
+//  ComakershipsAppTests
+//
+//  Created by Hossam Chakroun on 11/12/2020.
+//
+
+import XCTest
+@testable import ComakershipsApp
+
+class ComakershipsAppTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
